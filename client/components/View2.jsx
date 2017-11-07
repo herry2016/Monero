a2ab6c584c12d444e766e9b324e08a719d6bfda2
Jay Kim <jay2340@gmail.com>
2017-11-07 15:34:24 -0800

    completed app components

---
+++ client/components/View2.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { Button } from 'react-bootstrap'
+
+
+class View2 extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+
+    return (
+      <div>
+        View2
+      </div>
+    )
+  }
+}
