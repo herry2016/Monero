9468b6d183830cf395bf60634a98b736382cf3b4
balex8888 <balex8888@gmail.com>
2017-11-07 15:39:52 -0800

    Slight server change test

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const parser = require('body-parser');
 const path = require('path');
+// Alex Test
+//My code is here
 const db = require('../database/db.js');
 
 const app = express();
 const router = express.Router();
 
 port = 9001;
+
+
+
 
 router.get('/', function(req, res) {
   res.json({message: 'Router api working'})
