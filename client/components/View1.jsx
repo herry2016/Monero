faf0ecd6e4bcca9798de456873bd35e33dab55d4
Vestige717 <Vestige717@gmail.com>
2017-11-08 10:45:17 -0800

    testing

--- client/components/View1.jsx
+++ client/components/View1.jsx
@@ -13,11 +13,21 @@ export default class View1 extends Component {
   }
 
   render() {
-
+    var sectionStyle = {
+      width: '100%',
+      height: '100%',
+      backgroundImage: "url('https://www.monero.how/photos/monero-coin7-1024x909.jpg')",
+      repeat: "no-repeat"
+    }
     return (
-      <div>
+      
+      <div id="viewOne" style={sectionStyle}>
+       
         View1
       </div>
     )
   }
 }
+
+// {document.getElementById("viewOne").appendChild(elem)}
+// {elem.src = 'https://www.monero.how/photos/monero-coin7-1024x909.jpg'}
