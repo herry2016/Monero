52446aa89337bf2ebf9ab6e4ef0e809040894bdf
Jay Kim <jay2340@gmail.com>
2017-11-07 19:50:41 -0800

    added login and signup button/rendered page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,24 +3,23 @@ import ReactDOM from 'react-dom'
 import { Button } from 'react-bootstrap'
 import View1 from './View1.jsx'
 import View2 from './View2.jsx'
+import Login from './Login.jsx'
+import Signup from './Signup.jsx'
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.handleClickButton1 = this.handleClickButton1.bind(this)
-    this.handleClickButton2 = this.handleClickButton2.bind(this)
+    this.handleClickButton = this.handleClickButton.bind(this)
+
 
     this.state = {
       viewing: 1
     };
   }
 
-  handleClickButton1() {
-    this.setState({ viewing: 1 }, () => { console.log('new state: ', this.state) });
-  }
-
-  handleClickButton2() {
-    this.setState({ viewing: 2 }, () => { console.log('new state: ', this.state) });
+  handleClickButton(pageNum) {
+    console.log('pageNum', pageNum)
+    this.setState({ viewing: pageNum }, () => { console.log('new state: ', this.state, pageNum) });
   }
 
   render() {
@@ -28,12 +27,18 @@ class App extends Component {
     return (
       <div>
         <div>
-          <Button bsStyle="warning" bsSize="large" onClick={this.handleClickButton1}>View1</Button>
-          <Button bsStyle="info" bsSize="large" onClick={this.handleClickButton2}>View2</Button>
+          <Button id="form" bsStyle="default" bsSize="small" onClick={this.handleClickButton.bind(null, 3)}>Login</Button>
+          <Button id="form" bsStyle="default" bsSize="small" onClick={this.handleClickButton.bind(null, 4)}>Signup</Button>
+        </div>
+        <div>
+          <Button bsStyle="warning" bsSize="large" onClick={this.handleClickButton.bind(null, 1)}>View1</Button>
+          <Button bsStyle="info" bsSize="large" onClick={this.handleClickButton.bind(null, 2)}>View2</Button>
         </div>
         <div>
           {this.state.viewing === 1 ? <div><View1 /></div> : null}
           {this.state.viewing === 2 ? <div><View2 /></div> : null}
+          {this.state.viewing === 3 ? <div><Login /></div> : null}
+          {this.state.viewing === 4 ? <div><Signup /></div> : null}
         </div>
       </div>
     )
