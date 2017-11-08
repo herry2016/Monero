54b0c369653468b8fd36fc8e2f549e2001cf1a7e
balex8888 <balex8888@gmail.com>
2017-11-07 16:29:58 -0800

    changes merged

--- server/server.js
+++ server/server.js
@@ -2,15 +2,14 @@ const express = require('express');
 const parser = require('body-parser');
 const path = require('path');
 // Alex Test
-//My code is here
+// My code is here
 const db = require('../database/db.js');
 
 const app = express();
 const router = express.Router();
 
 port = 9001;
-
-
+app.use(express.static(path.resolve(__dirname + '../client')))
 
 
 router.get('/', function(req, res) {
@@ -25,7 +24,7 @@ router.use(function(req, res, next) {
 
 
 
-app.use('/api', router);
+app.use('/main', router);
 app.listen(port, () => {
   console.log('Server is running on port: ' + port)
 })
