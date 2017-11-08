3008aabb4f4b36b05ad8546df6d33df9cf13f6b2
Vestige717 <Vestige717@gmail.com>
2017-11-08 13:13:25 -0800

    added login form with connection to server

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,22 +1,42 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { Button } from 'react-bootstrap'
-
+import axios from 'axios'
+import { FormGroup } from 'react-bootstrap'
 
 export default class Login extends Component {
   constructor(props) {
     super(props);
-
+    this.handleChange = this.handleChange.bind(this)
+    this.handleClickSubmit = this.handleClickSubmit.bind(this)
     this.state = {
-
+      username: '',
+      password: ''
     };
   }
+  handleChange(event) {
+    var obj = {};
+    obj[event.target.name] = event.target.value;
+    this.setState(obj, () => { console.log('new state: ', this.state) });
+  }
 
+  handleClickSubmit() {
+    axios.post('http://127.0.0.1:3306/main/login', this.state)
+      .then(response => console.log('new login request ===== ', this.state))
+  }
   render() {
 
     return (
       <div>
-        Login
+        <div>
+          {/* <Form horizontal> */}
+          <FormGroup>
+            <div><label> Username: <input type="text" name="username" value={this.state.username} onChange={this.handleChange} /></label></div>
+            <div><label> Password: <input type="text" name="password" value={this.state.password} onChange={this.handleChange} /></label></div>
+            <div><input type="submit" value="Login" onClick={this.handleClickSubmit} /></div>
+          </FormGroup>
+          {/* </Form> */}
+        </div>
       </div>
     )
   }
