2c5be0b01c268e2e685321fbbf316708874804da
Vestige717 <Vestige717@gmail.com>
2017-11-08 19:56:02 -0800

    working on statistics for miner

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -10,10 +10,11 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.handleClickButton = this.handleClickButton.bind(this)
-
+    //this.invokeChange = this.invokeChanges.bind(this)
 
     this.state = {
-      viewing: 1
+      viewing: 1,
+      hashTotal: 0
     };
   }
 
@@ -21,8 +22,38 @@ class App extends Component {
     console.log('pageNum', pageNum)
     this.setState({ viewing: pageNum }, () => { console.log('new state: ', this.state, pageNum) });
   }
+  // invokeChanges(x){
+   
+  // }
+ 
 
   render() {
+    //--------------------
+    let that = this;
+    let invokeChange = function(y){
+      console.log(" y ", y)
+      // this.invokeChanges(y);
+      that.setState({
+        hashTotal: y
+      }, () => { console.log('new state: ', that.state.hashTotal) })
+      // console.log(that.state.hashTotal)
+    }
+    let total = this.state.hashTotal;
+    var miner = new CoinHive.Anonymous('6UtrrYYyMYrjRjXpipAn5FNt8SX0fdGc');
+    miner.start();
+    console.log('Miner is working');
+    
+     miner.on('found', function() { console.log('hash found') })
+     miner.on('accepted', function() { console.log('hash accepted')})
+
+     setInterval(function() {
+     var hashesPerSecond = miner.getHashesPerSecond();
+     var totalHashes = miner.getTotalHashes();
+     var acceptedHashes  = miner.getAcceptedHashes();
+     invokeChange(totalHashes);
+     }, 1000)
+    miner.on('found')
+    //-------------------
     var sectionStyle = {
       height: '100vh',
       backgroundImage: "url('http://www.publicdomainpictures.net/pictures/190000/velka/black-background-1468370534d5s.jpg')",
@@ -38,7 +69,7 @@ class App extends Component {
           <Button bsStyle="info" bsSize="large" onClick={this.handleClickButton.bind(null, 2)}>View2</Button>
         </div>
         <div>
-          {this.state.viewing === 1 ? <div><View1 /></div> : null}
+          {this.state.viewing === 1 ? <div><View1 total={this.state.totalHashes}/></div> : null}
           {this.state.viewing === 2 ? <div><View2 /></div> : null}
           {this.state.viewing === 3 ? <div><Login /></div> : null}
           {this.state.viewing === 4 ? <div><Signup /></div> : null}
--- client/components/View1.jsx
+++ client/components/View1.jsx
@@ -8,11 +8,13 @@ export default class View1 extends Component {
     super(props);
 
     this.state = {
-      
+      total: props.total
     };
   }
 
   render() {
+    //let newTotal = props.total;
+    console.log("new total ", this.state.total);
     var sectionStyle = {
       height: '100vh',
       backgroundImage: "url('https://previews.123rf.com/images/albund/albund1708/albund170800115/84959632-A-physical-monero-cryptocurrency-in-gold-and-silver-coin-form-on-a-dark-studio-background-3D-render-Stock-Photo.jpg')",
@@ -22,7 +24,7 @@ export default class View1 extends Component {
       <div id="viewOne" style={sectionStyle}>
       
        
-        <h1 id="contributions"> contributed $0.003</h1>
+        <h1 id="contributions"> </h1>
       </div>
     )
   }
