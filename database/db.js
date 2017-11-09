5e0c9bfbf0baa8de59f6c57f0d9fcf55a896f17a
balex8888 <balex8888@gmail.com>
2017-11-08 16:11:43 -0800

    Added MongoDB refactor

--- database/db.js
+++ database/db.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize')
 const db = new Sequelize('database', null, null, {
     host: 'localhost',
-    dialect: 'sqlite',
+    dialect: 'mysql',
     // only if you  use sqlite
-    storage: '../database/storage.sqlite'
+    // storage: '../database/storage.sqlite'
 })
 
 const Users = db.define('users',{
@@ -18,3 +18,31 @@ const Users = db.define('users',{
 
 Users.sync();
 exports.Users = Users;
+
+
+
+//// MONGODB Refactor - Leave Commented for now
+// const mongoose = require('mongoose')
+// const dbCon = mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds151955.mlab.com:51955/monerousers', { useMongoClient: true })
+// //// const dbCon = mongoose.connect('mongodb://localhost/monerousers', { useMongoClient: true }) //Using mLabs Database instead so leave this commented out
+// mongoose.Promise = global.Promise;
+
+// dbCon.on('error', function(err) {
+//   console.log('Mongoose had connection ERROR: ' + err);
+// })
+// dbCon.on('connected', function() {
+//   console.log('MongooseDB is CONNECTED');
+// })
+
+// const userSchema = new mongoose.Schema({
+//   username: String,
+//   password: String,
+//   email: String,
+//   totalhashes: Number,
+//   totaltime: Number,
+//   totalamount: Number,
+
+// });
+//  const Users = mongoose.model('Users', userSchema);
+
+//  module.exports = {dbCon, Users}
