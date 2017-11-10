b368306886b8d9b69af3ed70b7d2f63f1f56d104
Jay Kim <jay2340@gmail.com>
2017-11-10 15:40:52 -0800

    no change

--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,7 @@ module.exports = {
         test: /\.(js|jsx)$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
-        query: { presets: ['es2015', 'react'],
-                  plugins: ['transform-class-properites'] }
+        query: { presets: ['es2015', 'react'] }
       }
     ]
   }
